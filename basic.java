97bff7db19f5ceebcdfa473bf542a1ecd1400461
Jordan Leturgez <jordanleturgez@gmail.com>
2026-02-03 03:30:53 -0500

    Add factorial function with test cases

---
+++ basic.java
@@ -0,0 +1,13 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
+
+# Test cases
+print(factorial(2))   # 2
+print(factorial(3))   # 6
+print(factorial(5))   # 120
+print(factorial(10))  # 3628800
